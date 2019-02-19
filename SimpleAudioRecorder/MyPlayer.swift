3281fe33f731d6b0ddbcb9584efa6c36686f7f7d
iyinraphael <iyinraphael@gmail.com>
2019-02-19 09:32:09 -0800

    initial commit

---
+++ SimpleAudioRecorder/MyPlayer.swift
@@ -0,0 +1,39 @@
+ //
+//  MyPlayer.swift
+//  SimpleAudioRecorder
+//
+//  Created by Iyin Raphael on 2/19/19.
+//  Copyright © 2019 Iyin Raphael. All rights reserved.
+//
+
+import Foundation
+import AVFoundation
+ 
+
+ class Player: NSObject {
+    
+    private var audioPlayer: AVAudioPlayer?
+    
+    var isPlaying: Bool {
+        return audioPlayer?.isPlaying ?? false
+    }
+    
+    func playPause() {
+        if isPlaying {
+            pause()
+        } else {
+            play()
+        }
+    }
+    
+    func play(){
+        if audioPlayer == nil {
+            //make an audio player
+        }
+        audioPlayer?.play()
+    }
+    
+    func pause(){
+        audioPlayer?.pause()
+    }
+ }
---
+++ SimpleAudioRecorder/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  SimpleAudioRecorder
+//
+//  Created by Iyin Raphael on 2/19/19.
+//  Copyright © 2019 Iyin Raphael. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var playButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func tappedPLayButton(_ sender: Any) {
+    }
+    
+}
+
